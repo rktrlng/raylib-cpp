82c9f11eb92f2f828287d10c1f91e0052b6265bb
rktrlng <rik.teerling@gmail.com>
2022-03-06 14:47:20 +0100

    test2

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!!";
+        const char* text = "OMG! IT WORKS!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
